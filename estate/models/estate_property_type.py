de470ad50afb75afe126002bead12e2828d36106
Lucas Abba <abbalucas4@gmail.com>
2022-07-06 22:18:53 -0300

    inline view type view

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -5,6 +5,7 @@ class EstatePropertyType(models.Model):
     _description = 'property type'
 
     name = fields.Char(required=True, default="Unknown")
+    property_ids = fields.One2many('estate.property', 'property_type_id', readonly=True)
 
     _sql_constraints = [
         ('type_name_uniq', 'unique (name)',
